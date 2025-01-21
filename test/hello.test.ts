63d00b6d3f41d12e8ab75a6fd958bddedb3a69db
Eduardo R Vieira <eduardovieirayt@gmail.com>
2025-01-21 15:42:19 -0300

    delete: delete mock files

--- test/hello.test.ts
+++
@@ -1,9 +0,0 @@
-import { assert } from "console"
-import { hello } from "../src/expense/functions/handler"
-
-describe('Example Test', () => {
-    it('should return a success response', () => {
-      expect(1 + 1).toBe(2);
-    });
-});
-  
